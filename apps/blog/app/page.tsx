570ebc04634e4d4ed6dfddc2a32925b273cd22b5
Franciszek Pawlak <franciszek.pawlak@erecruiter.pl>
2026-08-31 12:31:01 +0200

    Hide section

--- apps/blog/app/page.tsx
+++ apps/blog/app/page.tsx
@@ -3,11 +3,13 @@ import Links from "~/components/home/Links";
 import Me from "~/components/home/Me";
 import Creator from "~/components/home/Creator";
 
+const SHOW_CIERPIENIE_SECTION = false;
+
 export default function HomePage() {
 	return (
 		<div>
 			<Me />
-			<Creator />
+			{SHOW_CIERPIENIE_SECTION && <Creator />}
 			<Slogan />
 			<Links />
 		</div>
